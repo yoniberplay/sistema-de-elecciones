4c5e10aacb1f9dbe461ada1229f02eb97090c576
Maycol Perez <maycoldanielp@gmail.com>
2023-04-03 21:51:37 -0400

    ciudadano auth middleware added

--- routes/votacionRouter.js
+++ routes/votacionRouter.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const VotacionController = require('../controllers/VotacionController');
 
-const requireAuth = require('../middleware/is-auth')
+const requireAuth = require('../middleware/ciudadano-auth')
 
-
-//! Creacion de middleware para que solo una session.ciudadano pueda accder
 //! y luego que valide si ya ha votado por alguno para redirigirlo solo a donde no haya votado
-router.get('/votacion', VotacionController.getVotacionPage);
+router.get('/votacion', requireAuth, VotacionController.getVotacionPage);
 
 module.exports = router;
