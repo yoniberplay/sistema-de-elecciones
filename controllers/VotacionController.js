b507e641c7490c81e54e08b38d86b9ef8b19cd37
Maycol Perez <maycoldanielp@gmail.com>
2023-04-08 19:48:14 -0400

    view vote dinamic front created

--- controllers/VotacionController.js
+++ controllers/VotacionController.js
@@ -1,18 +1,66 @@
+const Candidato = require("../models/Candidato");
+const Partido = require("../models/Partido");
+
+const Puesto = require("../models/Puesto")
+const Eleccion = require("../models/Elecciones")
+
 
 exports.getPage = (req, res) => {
-    res.render('ciudadano/votacion', {
-        pageTitle: 'Votacion',
-    })
-}
+  res.render("ciudadano/votacion", {
+    pageTitle: "Votacion",
+  });
+};
 
 exports.getVotacionPage = (req, res, next) => {
-    
-    if (!req.session.eleccion) {
-        req.flash("errors", "No hay ninguna eleccion activa.");
-    }        
-     res.status(200).render('ciudadano/votacion', {
-        pageTitle: 'Votacion',
-        ciudadano: req.ciudadano.dataValues,
-        eleccion: req.session.eleccion, 
+  if (!req.session.eleccion) {
+    req.flash("errors", "No hay ninguna eleccion activa.");
+  }
+  res.status(200).render("ciudadano/votacion", {
+    pageTitle: "Votacion",
+    ciudadano: req.ciudadano.dataValues,
+    eleccion: req.session.eleccion,
+    votacionActive: true,
+  });
+};
+
+exports.puestosNoVotadosCiudadano = (req, res) => {
+  const PuestosNoVotados = req.session.puestosNoVotados;
+
+  if (!PuestosNoVotados || PuestosNoVotados.length < 1) {
+    return res.json({
+      ok: true,
+      hasPuestos: false,
+      mensaje: "no hay puestos por votar",
+    });
+  }
+
+  return res.json({
+    ok: true,
+    hasPuestos: true,
+    PuestosNoVotados,
+  });
+};
+
+exports.GetCandidatoList = async (req, res, next) => {
+    const puestoId = req.params.puestoId
+
+    if (!puestoId) {
+        return res.json({
+            ok: false,
+            msg: 'No se ha enviado el puesto'
+        })
+    }
+
+    const candidatos = await Candidato.findAll({
+        raw: true, 
+        where: {PuestoId: puestoId},
+        include: [{model: Partido}]
     })
-}
+
+
+    return res.json({
+        candidatos: candidatos,
+        hasCandidatos: candidatos.length > 0
+    })
+
+};
---
+++ public/assets/js/site/votacionComponents/printCandidato.js
@@ -0,0 +1,79 @@
+
+function printCandidato(candidato) {
+    const posicionesContainer = document.querySelector("#posicionesContainer");
+    posicionesContainer.innerHTML = "";
+  
+    const section = document.createElement("section");
+    section.className = "py-2";
+  
+    const container = document.createElement("div");
+    container.className = "container px-4 px-lg-5 my-5";
+  
+    const row = document.createElement("div");
+    row.className = "row gx-4 gx-lg-5 align-items-center";
+  
+    const colImg = document.createElement("div");
+    colImg.className = "col-md-6";
+  
+    const img = document.createElement("img");
+    img.className = "card-img-top mb-5 mb-md-0";
+    img.src = candidato.imgPerfil;
+    img.alt = `${candidato.name} img`;
+  
+    colImg.appendChild(img);
+  
+    const colDetails = document.createElement("div");
+    colDetails.className = "col-md-6";
+  
+    const h1 = document.createElement("h1");
+    h1.className = "display-5 fw-bolder text-white";
+    h1.textContent = `Nombre: ${candidato.name}`;
+  
+    const div = document.createElement("div");
+    div.className = "fs-5 mb-5";
+  
+    const span1 = document.createElement("span");
+    span1.className = "text-white";
+    span1.textContent = "Partido: ";
+  
+    const span2 = document.createElement("span");
+    span2.className = "text-white";
+    span2.textContent = candidato['Partido.name'];
+  
+    div.appendChild(span1);
+    div.appendChild(span2);
+  
+    const imgPartido = document.createElement("img");
+    imgPartido.className = "img-partido-detalle";
+    imgPartido.src = candidato['Partido.imgLogo'];
+    imgPartido.alt = `${candidato['Partido.name']} img`;
+  
+    const p = document.createElement("p");
+    p.className = "lead text-white";
+    p.textContent = `Descripcion partido: ${candidato['Partido.description']}`;
+  
+    const divBtn = document.createElement("div");
+    divBtn.className = "d-flex";
+  
+    const button = document.createElement("button");
+    button.className = "btn btn-outline-light flex-shrink-0";
+    button.type = "button";
+    button.textContent = "Votar";
+  
+    divBtn.appendChild(button);
+  
+    colDetails.appendChild(h1);
+    colDetails.appendChild(div);
+    colDetails.appendChild(imgPartido);
+    colDetails.appendChild(p);
+    colDetails.appendChild(divBtn);
+  
+    row.appendChild(colImg);
+    row.appendChild(colDetails);
+  
+    container.appendChild(row);
+  
+    section.appendChild(container);
+  
+    posicionesContainer.appendChild(section);
+  }
